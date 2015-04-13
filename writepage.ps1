8185afaf465642984db99b830cce7ee1d7fd15f9
bklockwood <bryan@lockwood.us>
2015-04-12 18:02:40 -0700

    remove old code (alligator/sparklines)

--- writepage.ps1
+++ writepage.ps1
@@ -13,130 +13,6 @@ The full path and name of file to write
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
-function Write-PageHeader
-{
-    [CmdletBinding()]
-    Param
-    (
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)][string]$FilePath
-    )
-
-    Begin{}
-    Process
-    {
-        $PageHead = @"
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title></title>
-  <script type="text/javascript" src="https://rawgithub.com/Alligator/sparklines/master/sparklines.js"></script>
-  <style type="text/css">
-  @import url(http://fonts.googleapis.com/css?family=Crimson+Text);
-  body {
-    font-family: 'Crimson Text', serif;
-    font-size: 14pt;
-    color: #444;
-    line-height: 1.35;
-  }
-
-  .page {
-    width: 30em;
-    margin: 0 auto;
-    margin-top: 2em;
-  }
-
-  .sparkline svg {
-    stroke: mediumseagreen;
-    stroke-width: 1.5px;
-  }
-  .sparkline-filled svg {
-    stroke: LightCoral;
-    fill: rgba(60,179,113,0.25);
-  }
-
-  .cpu sparkline-filled svg {
-    stroke: LightCoral;
-    fill: LightSalmon
-  }
-
-  .sparkline-mem svg {
-    stroke: LightCoral;
-    fill: LightSalmon
-  }
-
-  .sparkline-disk svg {
-    stroke: LightCoral;
-    fill: LightSalmon
-  }
-
-  .sparkline-net svg {
-    stroke: LightCoral;
-    fill: LightSalmon
-  }
-
-  pre, code {
-    font-size: 12pt;
-    white-space: pre-wrap;
-  }
-  
-  h1 {
-    border-bottom: 1px solid #ccc;
-    padding-bottom: .5em;
-    font-size: 1em;
-    text-align: center;
-    text-transform: uppercase;
-    font-weight: normal;
-    letter-spacing: .3em;
-  }
-
-.tftable {font-size:20px;color:#333333;width:100%;border-width: 1px;border-color: #a9a9a9;border-collapse: collapse;}
-.tftable th {font-size:20px;background-color:#E6E6E6;border-width: 1px;padding: 8px;border-style: solid;border-color: #a9a9a9;text-align:left;}
-.tftable tr {background-color:#FFFFFF;}
-.tftable td {font-size:20px;border-width: 1px;padding: 8px;border-style: solid;border-color: #a9a9a9;}
-
-
-  .footer {
-    border-top: 1px solid #ccc;
-    text-align: center;
-    padding-top: .5em;
-    color: #aaa;
-  }
-
-  a {
-    text-decoration: none;
-    color: mediumseagreen;
-  }
-  a:hover {
-    color: darkseagreen;
-  }
-  </style>
-</head>
-<body>
-  <div class="page">
-    <h1>Testing</h1>
-    <table class="tftable" border="1">
-    <tr><th>Servername</th><th>CPU</th><th>Mem</th><th>Disk</th><th>Net</th></tr>
-"@
-
-        out-file -InputObject $PageHead -FilePath $FilePath -Encoding unicode -Force
-    }
-    End{}
-
-}
-
-<#
-.Synopsis
-   Short description
-.DESCRIPTION
-   Long description
-.PARAMETER FilePath
-The full path and name of file to write
-.EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
-#>
 function Write-SparkyPageHeader
 {
     [CmdletBinding()]
@@ -485,45 +361,6 @@ Help for Param1
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
-function Write-PageLine
-{
-    [CmdletBinding()]
-    Param
-    (
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)][string]$LineName,
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=1)][string]$Data1,
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=2)][string]$Data2,
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=3)][string]$Data3,
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=4)][string]$Data4,
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=5)][string]$FilePath
-    )
-
-    Begin{}
-    Process
-    {
-        [string]$Line = "<tr><td>$LineName</td>"
-        [string]$Line += "<td> <span class=""sparkline cpu sparkline-filled"" data-values = ""$Data1""></span></td>"
-        [string]$Line += "<td> <span class=""sparkline sparkline-filled"" data-values = ""$Data2""></span></td>"
-        [string]$Line += "<td> <span class=""sparkline sparkline-filled"" data-values = ""$Data3""></span></td>"
-        [string]$Line += "<td> <span class=""sparkline sparkline-filled"" data-values = ""$Data4""></span></td>"
-        [string]$Line += "</tr>"
-        out-file -FilePath $FilePath -Encoding unicode -Append -NoClobber -InputObject $Line
-    }
-    End{}
-}
-
-<#
-.Synopsis
-   Short description
-.DESCRIPTION
-   Long description
-.PARAMETER Param1
-Help for Param1
-.EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
-#>
 function Write-SparkyPageLine
 {
     [CmdletBinding()]
@@ -545,9 +382,7 @@ function Write-SparkyPageLine
 
         [string]$Line += "`t <span class=""sparkline"" `r`n" 
         [string]$Line += "`tdata-preset=""cpu"" `r`n"
-        [string]$Line += "`tdata-points = ""$cpu"" >`r`n "
-
-        
+        [string]$Line += "`tdata-points = ""$cpu"" >`r`n "        
         [string]$Line += "`t</span> `r`n"
         [string]$Line += "`t12 `r`n" #<---not <var>
         [string]$Line += "`r`n"
@@ -586,24 +421,6 @@ $disk = Get-RandomArray -ArrayLength 144
 $net = Get-RandomArray -ArrayLength 144
 Write-SparkyPageLine -LineName "Server1" -cpu $cpu -mem $mem -disk $disk -net $net -FilePath $FilePath
 Write-SparkyPageFooter $FilePath
-
-<#
-$Values1 = Get-RandomArray -ArrayLength 144
-$Values2 = Get-RandomArray -ArrayLength 144
-$Values3 = Get-RandomArray -ArrayLength 144
-$Values4 = Get-RandomArray -ArrayLength 144
-Write-PageLine -LineName "Server2" -Data1 $Values1 -Data2 $Values2 -Data3 $Values3 -Data4 $Values4 -FilePath $FilePath
-$Values1 = Get-RandomArray -ArrayLength 144
-$Values2 = Get-RandomArray -ArrayLength 144
-$Values3 = Get-RandomArray -ArrayLength 144
-$Values4 = Get-RandomArray -ArrayLength 144
-Write-PageLine -LineName "Server3" -Data1 $Values1 -Data2 $Values2 -Data3 $Values3 -Data4 $Values4 -FilePath $FilePath
-$Values1 = Get-RandomArray -ArrayLength 144
-$Values2 = Get-RandomArray -ArrayLength 144
-$Values3 = Get-RandomArray -ArrayLength 144
-$Values4 = Get-RandomArray -ArrayLength 144
-Write-PageLine -LineName "Server4" -Data1 $Values1 -Data2 $Values2 -Data3 $Values3 -Data4 $Values4 -FilePath $FilePath
-#>
 
 
 
