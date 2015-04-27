d95d5f8f53686bc0c6da1eccfe335d51f304aec0
bklockwood <bryan@lockwood.us>
2015-04-26 19:51:40 -0700

    new function Write-CurrentPerfTable ...test output
    Test output to console only. Next start building the string output.

--- PSPerf.ps1
+++ PSPerf.ps1
@@ -1,11 +1,9 @@
-
 <#
 .Synopsis
    Gets a specific set of perfmon counters from local or remote computer.
    Returns a hashtable containing the data.
 .DESCRIPTION
    The returned hashtable includes:
-
    CpuQueue, \System\Processor Queue Length
    PagesPerSec, \Memory\Pages Input/Sec
    DiskQueues, a hashtable containing on or more of the following objects:
@@ -16,35 +14,25 @@
    
    "0 c:"     (disk 0 contains partition c:)
    "1 e: f:"  (disk 1 contains partitions e: and f:)
-
 .PARAMETER ComputerName
 The computer to get perfmon counter data from.
 .EXAMPLE
 PS> $pdata = get-perfdata -ComputerName s1
-
 PS> $pdata
-
 Name                           Value
 ----                           -----
 DiskQueues                     @{0 c:=0.27; 1 e: f:=0; _total=0.27} 
 CpuQueue                       1
 PagesPerSec                    0
-
-
 PS> $pdata.DiskQueues
-
 $pdata.DiskQueues
-
 0 c:      1 e: f:       _total
 ----      -------       ------
 0.27      0             0.27
-
 PS> $pdata.DiskQueues._total
 0
-
 PS> $pdata.CpuQueue
 1
-
 #>
 function Get-PerfData #queries a single computer for performance data
 {
@@ -95,7 +83,7 @@ function Get-PerfData #queries a single computer for performance data
 .EXAMPLE
    TBD
 #>
-function Add-Perfdata #Adds get-perfdata output into a hashtable of previously collected data
+function Add-PerfData #Adds get-perfdata output to hashtable of previously collected data
 {
     [CmdletBinding()]
     #[OutputType([int])]
@@ -140,15 +128,74 @@ function Add-Perfdata #Adds get-perfdata output into a hashtable of previously c
     End{}
 }
 
-if (!$StorageHash) {$StorageHash = @{}}
+<#
+.Synopsis
+   Reads from hashtable of collected data and writes a web formatted table of
+   sparklines.
+.DESCRIPTION
+   TBD
+.PARAMETER DataStore
+   The storage hash output from Add-PerfData
+.PARAMETER Path
+   Full path of file (usually a *.html) to output to.
+.EXAMPLE
+   Example of how to use this cmdlet
+
+#>
+function Write-CurrentPerfTable
+{
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, Position=0)]
+        [Alias("InputObject")][hashtable]$DataStore,        
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, Position=1)]
+        [Alias("OutputFile")][string]$Path
+    )
+
+    Begin{}
+    Process {
+        #start walking down the object. this should be recursive but I am lazy
+        foreach ($key in $DataStore.Keys) {
+            if ($DataStore.$key.GetType() -eq [System.Collections.Hashtable] ) {
+                write-host $key
+            } else {
+                write-host $key $datastore.$key
+            }
+            foreach ($subkey in $DataStore.$key.Keys) {
+                if ($DataStore.$key.$subkey.GetType() -eq [System.Collections.Hashtable]) {
+                    write-host " " $subkey 
+                    foreach ($subsubkey in $DataStore.$key.$subkey.Keys) {
+                        write-host "  " $subsubkey $DataStore.$key.$subkey.$subsubkey
+                    } 
+                } else {
+                    write-host " " $subkey $DataStore.$key.$subkey
+                }
+            }
+        }
+    }
+
+    End{}
+}
+
+## ---------------------------------------Script starts here---------------------------------
+$datastore = "C:\Users\Bryan\Documents\WindowsPowerShell\PSPerf\datastore.clixml"
+if (!$StorageHash) {
+    if (get-item $datastore) {
+        $StorageHash = Import-Clixml -Path $datastore
+    } else {
+    $StorageHash = @{}
+    }
+}
 $computername = 's1'
 $pdata = Get-PerfData $computername
 add-perfdata -StorageHash $StorageHash -Computername $computername -PerfData $pdata
+Export-Clixml -InputObject $StorageHash -Path $datastore
 
 <# 
 Next steps:
  Store the table/object on disk with export-clixml
  Trim each array to latest 144 items
- Iterate storagehash ad created comma-separated text strings to write to page
-
+ Iterate storagehash and create comma-separated text strings to write to page
 #> 
