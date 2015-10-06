723ee2b8959ff836ff57c3fb6a1f468fc746af57
Bryan Lockwood <bryan@lockwood.us>
2015-10-06 05:28:33 -0700

    still not how I want things
    fix the trustedhost test in Test-PsRemoting

--- CredExperiments.ps1
+++ CredExperiments.ps1
@@ -99,8 +99,67 @@ VERBOSE: Testing connection to computer 'FAKENAME' failed: No such host is known
     param( 
         [Parameter(Mandatory = $true, Position=0)]$Computername,
         [Parameter(Position=1)][PSCredential]$Credential
-    ) 
+    )
+
+    #if the verbose switch is on, do all these detailed checks
+    if ($VerbosePreference -ne "SilentlyContinue") {
     
+        #Report whether we can PSRemote to localhost
+        Write-Host "Checking whether PSRemoting is enabled locally ..." -NoNewline
+        $psremotingenabled = $true
+        try {
+            $ErrorActionPreference = "Stop"
+            New-PSSession -ComputerName localhost -Name testsession | out-null
+        } catch {
+            Write-Host "NOT ENABLED (bad)"
+            $psremotingenabled = $false
+        }    
+        if ($psremotingenabled) {
+            Remove-PSSession -name testsession | out-null
+            Write-Host "ENABLED (good)."
+        }
+
+        #Report whether remote computer is in TrustedHosts
+        Write-Host "Looking for $Computername in TrustedHosts ... " -NoNewline
+        if ((get-item WSMan:\localhost\Client\TrustedHosts).Value -contains $Computername) {
+            THIS IS BUSTED, the Value is not an array/collection
+            Write-Host "FOUND (good)."
+        } else {
+            Write-Host "NOT FOUND (bad)."
+        }
+
+        #Report whether remote computer is pingable
+        Write-Host "Testing ping to $Computername ... " -NoNewline
+        $reachable = $true
+        try {
+            $ErrorActionPreference = "Stop"
+            $pingresult = test-connection -ComputerName $Computername -Count 1 
+        } catch {
+            Write-Host "FAIL (bad)."
+            $reachable = $false
+        }
+        if ($reachable) {Write-Host "SUCCESS (good)."}
+    
+        #Report whether WSMAN port 5985 is open
+        if ($reachable) {
+            $ip = $pingresult.IPV4Address.IPAddressToString
+            Write-Host "Testing connection to $Computername WSMAN port ... " -NoNewline
+            try {
+                $socket = new-object System.Net.Sockets.TcpClient($ip, 5985)
+            } catch {
+                Write-Host "ERROR (bad)."
+            }
+            if ($socket.Connected) {
+                Write-Host "OPEN (good)."
+                $socket.close()
+            } else {
+                Write-Host "CLOSED (bad)."
+                $socket.Close()
+            }
+        }
+     } #end "if ($VerbosePreference -neq "SilentlyContinue")"
+
+    #Now actually try a PSRemoting connection
     try { 
         $ErrorActionPreference = "Stop"
         if ($Credential) {
@@ -111,108 +170,135 @@ VERBOSE: Testing connection to computer 'FAKENAME' failed: No such host is known
     } catch { 
         $icmresult = $_ 
     } 
-
+    
     if ($icmresult -eq 1) {
         return $true 
-        #Function ends here if the Invoke-Command succeeded
     } else {
         Write-Warning "Could not connect to $Computername"
-        Write-Verbose "$icmresult"
-    }    
-    
-    #Report whether PSRemoting is enabled locally
-    $psremotingenabled = $true
-    try {
-        $ErrorActionPreference = "Stop"
-        New-PSSession -ComputerName localhost -Name testsession | out-null
-    } catch {
-        Write-Verbose "PSRemoting is NOT enabled locally."
-        $psremotingenabled = $false
-    }    
-    if ($psremotingenabled) {
-        Remove-PSSession -name testsession | out-null
-        Write-Verbose "PSRemoting is enabled locally"
+        Write-Host "$icmresult"
+        return $false
     }
-
-    #Report whether remote computer is in TrustedHosts
-    if ((get-item WSMan:\localhost\Client\TrustedHosts).Value -contains $Computername) {
-        Write-Verbose "$Computername is in TrustedHosts"
-    } else {
-        Write-Verbose "$Computername is NOT in TrustedHosts"
-    }
-
-    #Report whether remote computer is pingable
-    $reachable = $true
-    try {
-        $ErrorActionPreference = "Stop"
-        test-connection -ComputerName $Computername -Count 1 |out-null
-    } catch {
-        Write-Verbose "$_"
-        $reachable = $false
-    }
-    if ($reachable) {Write-Verbose "$Computername reachable on network"}
-
-   
 }
 
-function Write-PSCredFile {
+function Add-PSCredtext {
 <#
 .SYNOPSIS
-Create a textfile containing username and securestring, which can be made into a PSCredential object.
-.PARAMETER Computername
-Name of computer the cred will be used on.
-.NOTES
-TBD
-.EXAMPLE
-TBD
+Interactively prompt user for a computername, username and password.
+These are used to generate new lines added to PSPerf.ini
 #>
     [CmdletBinding()]
-    param( 
-        [Parameter(Mandatory=$true, Position=0)]$Computername
-    )  
+
+    #Prompt for the computer name
+    $Computername = Read-Host -Prompt "Enter the computer name."
+    if ((get-item WSMan:\localhost\Client\TrustedHosts).Value -notcontains $Computername) {
+        $yesno = Get-YNAnswer -Question "$Computername is not in TrustedHosts. Add it? (Y/N)"
+        if ($yesno -eq "Y") { Add-TrustedHost $Computername }
+    }
 
     #Prompt for the username
     $UserName = Read-Host -Prompt "Enter username to be used on $Computername"
 
     #Prompt for the password
     $SecurePass = Read-Host -Prompt "Enter password to be used on $Computername" -AsSecureString
+    $SecurePass = $SecurePass | ConvertFrom-SecureString
+    [string]$output = @"
 
-    #Prompt for the output filepath
-    $OutputFile = Read-Host -Prompt "Enter full path and name of filename to write. (Example: .\$Computername-Creds.txt)"
+[$Computername]
+username=$username
+securestring=$SecurePass
+"@
 
-    $Computername | out-file -FilePath $OutputFile
-    $username | out-file -FilePath $OutputFile -Append -NoClobber
-    $SecurePass | ConvertFrom-SecureString | Out-File -FilePath $OutputFile -Append -NoClobber
+    Write-Host "The following will be added to PSPerf.ini:"
+    Write-Host $output
+    $answer = Get-YNAnswer -Question "Is this OK? (Y/N)"
+    If ($answer -eq "Y") {
+        $output | out-file -FilePath .\psperf.ini -Encoding ascii -Append -Force
+    }
 
 }
 
-function New-PSCredObj {
+function Get-YNAnswer {
 <#
 .SYNOPSIS
-Build PSCredential object using textfile created with Write-PSCredFile.
-.PARAMETER InputFile
-Name of file created with Write-PSCredFile.
-.NOTES
-TBD
-.EXAMPLE
-TBD
+Ask a question. Constrain the answer to "Y" or "N".
+Warn and repeat question if user flubs it.
+.PARAMETER Question
+The question to ask.
+#>
+    Param ( [Parameter(Mandatory=$true)][string]$Question )
+
+    try {
+        [ValidateSet("Y", "N")]$answer = Read-Host -Prompt $Question
+    } catch {
+        Write-Warning "Your answer must be either Y or N. Please try again."
+        Get-YNAnswer $Question
+    }
+    $answer
+}
+
+function Add-TrustedHost {
+<#
+.SYNOPSIS
+Add a computer to the TrustedHosts list
+.PARAMETER ComputerName
+Name of computer to add.
+.LINK
+http://blogs.technet.com/b/heyscriptingguy/archive/2013/11/29/remoting-week-non-domain-remoting.aspx
 #>
     [CmdletBinding()]
     param( 
-        [Parameter(Mandatory=$true, Position=0)]$InputFile
+        [Parameter(Mandatory=$true, Position=0)]$ComputerName
     ) 
 
-    $contents = get-content $InputFile
-    $Computername = $contents[0]
-    $Username = $contents[1]
-    $SecurePassword = $contents[2] | ConvertTo-SecureString
-    $NewCred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecurePassword
-    Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value $Computername -Concatenate
+    Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "$Computername" -Concatenate
+}
+
+function New-Cred {
+<#
+.SYNOPSIS
+Build a PSCredential object using username 
+and plaintext securestring.
+.PARAMETER UserName
+A username, typically in one of the following forms:
+username
+domainname\username
+computername\username
+username@somedomain.com
+.PARAMETER SecureString
+A plaintext securestring, often obtained via some construct such as:
+$PlainTextSecureString = $SecureString | ConvertFrom-SecureString
+.LINK
+http://social.technet.microsoft.com/wiki/contents/articles/4546.working-with-passwords-secure-strings-and-credentials-in-windows-powershell.aspx
+#>
+    [CmdletBinding()]
+    param( 
+        [Parameter(Mandatory=$true, Position=0)]$UserName,
+        [Parameter(Mandatory=$true, Position=1)]$SecureString
+    )
+    $NewCred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecureString
     $NewCred
 
 }
 
-break
-Test-PsRemoting ad1 -Credential $ad1cred -Verbose
-break
-Test-Uptime -ComputerName ad1 -verbose
+
+$config = Get-IniContent .\psperf.ini
+foreach ($target in ($config.targets.keys | where-object {$_ -notLike "Comment*" } | sort) ) {
+    $target
+    if (Test-PsRemoting $target) {write-host " $target SUCCESS PSRemoting"}
+    else {
+        $username = $config.$target.username
+        $securestring = $config.$target.securestring
+        if (($username -eq $null) -and ($securestring -eq $null)) {
+            Write-Host " no creds found for $target"
+        } else {
+            $securestring = $securestring | ConvertTo-SecureString
+            try {$cred = New-Cred -UserName $username -SecureString $securestring}
+            catch {write-host " Found creds, failed to convert to a cred object (bummer) $_"}
+            if ($cred) {Write-Host " Creds found for $target (good)."}
+            if (Test-PsRemoting $target $cred) {Write-Host " $target SUCCESS PSRemoting"}
+            else {Write-Host " $target still FAILS PSRemoting"}
+            
+        }
+
+    }
+}
